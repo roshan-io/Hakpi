bfb4fe444cf1df1800970138dcc04031f0c037a0
roshan-io <sheik.roshan.edu@gmail.com>
2025-05-04 11:06:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -137,27 +137,4 @@ document.querySelectorAll('.panel').forEach(panel => {
 
 
 
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.getElementById('fullscreenToggle');
-  const icon = document.getElementById('fsIcon');
 
-  function updateIcon() {
-    if (document.fullscreenElement) {
-      icon.classList.add('exit-mode');
-    } else {
-      icon.classList.remove('exit-mode');
-    }
-  }
-
-  toggle.addEventListener('click', () => {
-    if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen().catch(err => {
-        console.error("Fullscreen failed:", err);
-      });
-    } else {
-      document.exitFullscreen();
-    }
-  });
-
-  document.addEventListener('fullscreenchange', updateIcon);
-});
